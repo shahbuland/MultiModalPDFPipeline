51b00a64b9d5504adff02aa78c8f929b6a118e98
Shahbuland Matiana <ssmatian@uwaterloo.ca>
2023-11-16 17:02:30 -0500

    Add soft dictionary accessing for when keys dont match nougat output exactly

--- pdf_processing.py
+++ pdf_processing.py
@@ -16,8 +16,9 @@ def find_image_identifiers(text : str) -> Iterable[str]:
     - The key component here is the word Figure or Table, followed by a space, followed by a number, and then followed by a colon (:)
     - This script finds every such occurence and returns a list of the form [\"FigureX\", \"TableX\", ...]
     - These are used to associate the page with figures or tables found from the overall PDF
+    - X might be a decimal number (i.e. figure 1.2)
     """
-    pattern = r"(?:Figure|Table) \d+:"
+    pattern = r"(?:Figure|Table) \d+(?!\.\d+)?:"
     identifiers = re.findall(pattern, text)
 
     # Get rid of spaces, lowercase first letter, and remove colon from end
@@ -40,6 +41,44 @@ def extract_from_dict(d : dict, keys : Iterable[str]):
             del d[k]
     
     return return_keys, return_values
+
+def soft_extract_from_dict(d, keys):
+    """
+    Similar to the above function but with one important difference:
+    - In some cases Nougat doesn't extract a figures caption label correctly
+        (i.e. "Figure 1.2:" is read as "Figure 2:", which gives identifier "figure2")
+    - PDFFigures is able to correctly identify figures with decimals
+    - so mismatch between "figure2" and "figure1.2"
+    - This function implements a soft solution where if the key doesn't exist, we look for the first similar key
+    - This will likely cause issues when looking at anything that isn't a large PDF
+    """
+
+    return_keys = []
+    return_values = []
+    
+    # figure1.2 -> figure2
+    def delete_numbers_before_decimal(s):
+        decimal_index = s.find('.')
+        if decimal_index != -1:
+            non_digit_index = re.search("\d", s).start()
+            s = s[:non_digit_index] + s[decimal_index+1:]
+        return s
+    
+    for k in keys: # iterate through provided keys
+        if k in d:
+            return_keys.append(k)
+            return_values.append(d[k])
+            del d[k]
+        else:
+            for k_i in d.keys(): # iterate through dict
+                if delete_numbers_before_decimal(k_i) == k:
+                    return_keys.append(k)
+                    return_values.append(d[k_i])
+                    del d[k_i]
+                    break # found a match for key, break from looking through dict and onto next key
+    
+    return return_keys, return_values
+                
 
 class PDFProcessor:
     """
@@ -83,7 +122,7 @@ class PDFProcessor:
             raw_text = self.call_nougat(page_img)
             img_ids = find_image_identifiers(raw_text)
 
-            img_ids, imgs = extract_from_dict(figs, img_ids)
+            img_ids, imgs = soft_extract_from_dict(figs, img_ids)
 
             pdf_obj.add_page(
                 PDFPage(
